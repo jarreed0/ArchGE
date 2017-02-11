27b7708f8661012116d61e2bd03661bd0845d857
jarreed0 <jarreed0@gmail.com>
2017-02-11 10:43:18 -0700

    Began to work on SDL window class

---
+++ window.h
@@ -0,0 +1,19 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include "global.h"
+
+/*
+This class is to create and manage the window(s).
+Created by: Avery Reed on 2/10/17
+Last Edited by: Avery Reed 2/10/17
+*/
+
+class Window {
+public:
+  Window();
+  ~Window();
+private:
+};
+
+#endif //WIDNOW_H
