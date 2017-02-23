413f558ad090405bba6eea03e309dc71e2bec709
Avery Reed <jarreed0@gmail.com>
2017-02-23 10:17:38 -0700

    created center object function

--- testgames/genesis/genesis.cpp
+++ testgames/genesis/genesis.cpp
@@ -3,21 +3,18 @@
 Genesis::Genesis() : map(28) {
   engine.init(TITLE, WIDTH, HEIGHT, 0);
   running=true;
-  background.setImage("res/bkg.bmp", engine.renderScreen());
-  background.setSource(0, 0, WIDTH, HEIGHT);
-  background.setDest(WIDTH, HEIGHT, 0, 0);
+  engine.setBackground("res/bkg.bmp");
   player.setImage("res/player.bmp", engine.renderScreen());
   player.setSource(0, 0, TILE_SIZE, 45);
-  player.setDest(TILE_SIZE, 45,(WIDTH/2)-(TILE_SIZE/2), (HEIGHT/2)-(45/2));
+  player.center(WIDTH, HEIGHT);
   engine.setColor(0x00, 0x00, 0xff);
   map.setWindowSize(WIDTH, HEIGHT);
   map.setCameraMargin(75, 75, WIDTH, HEIGHT);
   map.centerLens(40);
-  vector<Tile> tmp;
-  tmp = map.loadMaps("wall", "res/avery.wall", "res/wall.bmp", engine.renderScreen(), TILE_SIZE, TILE_SIZE, 1, 7);
-  tmp = map.loadMaps("blocks", "res/avery.map", "res/blocks.bmp", engine.renderScreen(), TILE_SIZE, TILE_SIZE, 1, 22);
+  vector<Tile> tmp1, tmp2;
+  tmp1 = map.loadMaps("wall", "res/avery.wall", "res/wall.bmp", engine.renderScreen(), TILE_SIZE, TILE_SIZE, 1, 7);
+  tmp2 = map.loadMaps("blocks", "res/avery.map", "res/blocks.bmp", engine.renderScreen(), TILE_SIZE, TILE_SIZE, 1, 22);
   speed = 5;
-  int tick = 0;
   start();
 }
 Genesis::~Genesis() {
