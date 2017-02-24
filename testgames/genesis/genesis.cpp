98d5956bab3be9b06c61501c1723e28af20af120
Avery Reed <jarreed0@gmail.com>
2017-02-23 20:44:53 -0700

    working on tiling stumbling problem

--- testgames/genesis/genesis.cpp
+++ testgames/genesis/genesis.cpp
@@ -13,10 +13,10 @@ Genesis::Genesis() : map(28) {
   map.setCameraMargin(75, 75, WIDTH, HEIGHT);
   map.centerLens(40);
   engine.preLoop();
-  //vector<Tile> tmp1, tmp2;
-  //tmp1 = map.loadMaps("wall", "res/avery.wall", "res/wall.bmp", engine.renderScreen(), TILE_SIZE, TILE_SIZE, 1, 7);
-  //tmp2 = map.loadMaps("blocks", "res/avery.map", "res/blocks.bmp", engine.renderScreen(), TILE_SIZE, TILE_SIZE, 1, 22);
-  speed = 3;
+  vector<Tile> tmp1, tmp2;
+  tmp1 = map.loadMaps("wall", "res/avery.wall", "res/wall.bmp", engine.renderScreen(), TILE_SIZE, TILE_SIZE, 1, 7);
+  tmp2 = map.loadMaps("blocks", "res/avery.map", "res/blocks.bmp", engine.renderScreen(), TILE_SIZE, TILE_SIZE, 1, 22);
+  speed = 5;
   start();
 }
 Genesis::~Genesis() {
@@ -32,38 +32,42 @@ void Genesis::start() {
 }
 void Genesis::draw() {
   engine.pushToScreen(background);
-  /*tiles = map.getTilesToRender();
+  tiles = map.getTilesToRender();
   if(tiles.empty()) {
     cout << "No Tiles Loaded In!" << endl << "Trying To Reload Tiles." << endl;
-    vector<Tile> tmp1, tmp2;
+    /*vector<Tile> tmp1, tmp2;
     tmp1 = map.loadMaps("wall", "res/avery.wall", "res/wall.bmp", engine.renderScreen(), TILE_SIZE, TILE_SIZE, 1, 7);
     tmp2 = map.loadMaps("blocks", "res/avery.map", "res/blocks.bmp", engine.renderScreen(), TILE_SIZE, TILE_SIZE, 1, 22);
     if(tmp1.empty())  cout << "Tiles is still empty." << endl;
     if(tmp2.empty()) cout << "Tiles is still empty." << endl;
-    if(tiles.empty()) {
+    if(tiles.empty()) {*/
       cout << "Failed To Load In Tiles.\nAborting...\n";
       running = false;
-    }
+    //}
   }
   for(int i = 0; i<tiles.size(); i++) {
     engine.pushToScreen(tiles[i]);
-  }*/
+  }
   engine.pushToScreen(player);
   //engine.splash();
 }
 void Genesis::checkInput() {
   input.logPress();
   if(input.checkKey(input.right)) {
-    player.move(speed, 0);
+    map.move(speed, 0);
+    player.move(speed/2, 0);
   }
   if(input.checkKey(input.left)) {
-    player.move(-speed, 0);
+    map.move(-speed, 0);
+    player.move(-speed/2, 0);
   }
   if(input.checkKey(input.up)) {
-    player.move(0, speed);
+    map.move(0, speed);
+    player.move(0, speed/2);
   }
   if(input.checkKey(input.down)) {
-    player.move(0, -speed);
+    map.move(0, -speed);
+    player.move(0, -speed/2);
   }
   if(input.checkKey(input.quit)) running=false;
   if(input.checkKey(input.esc)) running=false;
