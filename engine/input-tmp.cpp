9785926bd71d25c286af67dfaaca14acabce3427
Cody Johns <cjohns@archeantus.net>
2017-03-16 12:18:14 -0500

    Input update.
    Engine input now supports multiple key presses. Engine now handles both key press and key release.

--- engine/input-tmp.cpp
+++ engine/input-tmp.cpp
@@ -46,9 +46,111 @@ Input::Input() {
 Input::~Input() {}
 
 void Input::logPress() {
-  bool done = reset();
+  //bool done = reset();
   SDL_Event event;
   while(SDL_PollEvent(&event)) {
+    if(event.type == SDL_QUIT) {keys[quit] = true; cout << "Quiting\n";}
+    if(event.type == SDL_MOUSEBUTTONDOWN) {
+      if((event.button.button == SDL_BUTTON_LEFT) && !keys[mouseleft]) {keys[mouseleft] = true; cout << "Left mouse button\n";}
+      if((event.button.button == SDL_BUTTON_RIGHT) && !keys[mouseright]) {keys[mouseright] = true; cout << "Right mouse button\n";}
+      if((event.button.button == SDL_BUTTON_MIDDLE) && !keys[mousemiddle]) {keys[mousemiddle] = true; cout << "Middle mouse button\n";}
+    }
+    if(event.type == SDL_MOUSEBUTTONUP) {
+      if((event.button.button == SDL_BUTTON_LEFT) && keys[mouseleft]) {keys[mouseleft] = false; cout << "Left mouse button up\n";}
+      if((event.button.button == SDL_BUTTON_RIGHT) && keys[mouseright]) {keys[mouseright] = false; cout << "Right mouse button up\n";}
+      if((event.button.button == SDL_BUTTON_MIDDLE) && keys[mousemiddle]) {keys[mousemiddle] = false; cout << "Middle mouse button up\n";}
+    }
+    if(event.type == SDL_KEYDOWN) {
+      if((event.key.keysym.sym == SDLK_LEFT) && !keys[left]) {keys[left] = true; cout << "left\n";}
+      if((event.key.keysym.sym == SDLK_RIGHT) && !keys[right]) {keys[right] = true; cout << "right\n";}
+      if((event.key.keysym.sym == SDLK_DOWN) && !keys[down]) {keys[down] = true; cout << "down\n";}
+      if((event.key.keysym.sym == SDLK_UP) && !keys[up]) {keys[up] = true; cout << "up\n";}
+      if((event.key.keysym.sym == SDLK_a) && !keys[a]) {keys[a] = true; cout << "a\n";}
+      if((event.key.keysym.sym == SDLK_b) && !keys[b]) {keys[b] = true; cout << "b\n";}
+      if((event.key.keysym.sym == SDLK_c) && !keys[c]) {keys[c] = true; cout << "c\n";}
+      if((event.key.keysym.sym == SDLK_d) && !keys[d]) {keys[d] = true; cout << "d\n";}
+      if((event.key.keysym.sym == SDLK_e) && !keys[e]) {keys[e] = true; cout << "e\n";}
+      if((event.key.keysym.sym == SDLK_f) && !keys[f]) {keys[f] = true; cout << "f\n";}
+      if((event.key.keysym.sym == SDLK_g) && !keys[g]) {keys[g] = true; cout << "g\n";}
+      if((event.key.keysym.sym == SDLK_h) && !keys[h]) {keys[h] = true; cout << "h\n";}
+      if((event.key.keysym.sym == SDLK_i) && !keys[i]) {keys[i] = true; cout << "i\n";}
+      if((event.key.keysym.sym == SDLK_j) && !keys[j]) {keys[j] = true; cout << "j\n";}
+      if((event.key.keysym.sym == SDLK_k) && !keys[k]) {keys[k] = true; cout << "k\n";}
+      if((event.key.keysym.sym == SDLK_l) && !keys[l]) {keys[l] = true; cout << "l\n";}
+      if((event.key.keysym.sym == SDLK_m) && !keys[m]) {keys[m] = true; cout << "m\n";}
+      if((event.key.keysym.sym == SDLK_n) && !keys[n]) {keys[n] = true; cout << "n\n";}
+      if((event.key.keysym.sym == SDLK_o) && !keys[o]) {keys[o] = true; cout << "o\n";}
+      if((event.key.keysym.sym == SDLK_p) && !keys[p]) {keys[p] = true; cout << "p\n";}
+      if((event.key.keysym.sym == SDLK_q) && !keys[q]) {keys[q] = true; cout << "q\n";}
+      if((event.key.keysym.sym == SDLK_r) && !keys[r]) {keys[r] = true; cout << "r\n";}
+      if((event.key.keysym.sym == SDLK_s) && !keys[s]) {keys[s] = true; cout << "s\n";}
+      if((event.key.keysym.sym == SDLK_t) && !keys[t]) {keys[t] = true; cout << "t\n";}
+      if((event.key.keysym.sym == SDLK_u) && !keys[u]) {keys[u] = true; cout << "u\n";}
+      if((event.key.keysym.sym == SDLK_v) && !keys[v]) {keys[v] = true; cout << "v\n";}
+      if((event.key.keysym.sym == SDLK_w) && !keys[w]) {keys[w] = true; cout << "w\n";}
+      if((event.key.keysym.sym == SDLK_a) && !keys[x]) {keys[x] = true; cout << "x\n";}
+      if((event.key.keysym.sym == SDLK_y) && !keys[y]) {keys[y] = true; cout << "y\n";}
+      if((event.key.keysym.sym == SDLK_z) && !keys[z]) {keys[z] = true; cout << "z\n";}
+      if((event.key.keysym.sym == SDLK_ESCAPE) && !keys[esc]) {keys[esc] = true; cout << "Esc\n";}
+      if((event.key.keysym.sym == SDLK_1) && !keys[1]) {keys[1] = true; cout << "1\n";}
+      if((event.key.keysym.sym == SDLK_2) && !keys[2]) {keys[2] = true; cout << "2\n";}
+      if((event.key.keysym.sym == SDLK_3) && !keys[3]) {keys[3] = true; cout << "3\n";}
+      if((event.key.keysym.sym == SDLK_4) && !keys[4]) {keys[4] = true; cout << "4\n";}
+      if((event.key.keysym.sym == SDLK_5) && !keys[5]) {keys[5] = true; cout << "5\n";}
+      if((event.key.keysym.sym == SDLK_6) && !keys[6]) {keys[6] = true; cout << "6\n";}
+      if((event.key.keysym.sym == SDLK_7) && !keys[7]) {keys[7] = true; cout << "7\n";}
+      if((event.key.keysym.sym == SDLK_8) && !keys[8]) {keys[8] = true; cout << "8\n";}
+      if((event.key.keysym.sym == SDLK_9) && !keys[9]) {keys[9] = true; cout << "9\n";}
+      if((event.key.keysym.sym == SDLK_0) && !keys[0]) {keys[0] = true; cout << "0\n";}
+      if((event.key.keysym.sym == SDLK_LSHIFT) && !keys[lshift]) {keys[lshift] = true; cout << "left shift\n";}
+      if((event.key.keysym.sym == SDLK_RSHIFT) && !keys[rshift]) {keys[rshift] = true; cout << "right shift\n";}
+    }
+    if(event.type == SDL_KEYUP) {
+      if((event.key.keysym.sym == SDLK_LEFT) && keys[left]) {keys[left] = false; cout << "left up\n";}
+      if((event.key.keysym.sym == SDLK_RIGHT) && keys[right]) {keys[right] = false; cout << "right up\n";}
+      if((event.key.keysym.sym == SDLK_DOWN) && keys[down]) {keys[down] = false; cout << "down up\n";}
+      if((event.key.keysym.sym == SDLK_UP) && keys[up]) {keys[up] = false; cout << "up up\n";}
+      if((event.key.keysym.sym == SDLK_a) && keys[a]) {keys[a] = false; cout << "a up\n";}
+      if((event.key.keysym.sym == SDLK_b) && keys[b]) {keys[b] = false; cout << "b up\n";}
+      if((event.key.keysym.sym == SDLK_c) && keys[c]) {keys[c] = false; cout << "c up\n";}
+      if((event.key.keysym.sym == SDLK_d) && keys[d]) {keys[d] = false; cout << "d up\n";}
+      if((event.key.keysym.sym == SDLK_e) && keys[e]) {keys[e] = false; cout << "e up\n";}
+      if((event.key.keysym.sym == SDLK_f) && keys[f]) {keys[f] = false; cout << "f up\n";}
+      if((event.key.keysym.sym == SDLK_g) && keys[g]) {keys[g] = false; cout << "g up\n";}
+      if((event.key.keysym.sym == SDLK_h) && keys[h]) {keys[h] = false; cout << "h up\n";}
+      if((event.key.keysym.sym == SDLK_i) && keys[i]) {keys[i] = false; cout << "i up\n";}
+      if((event.key.keysym.sym == SDLK_j) && keys[j]) {keys[j] = false; cout << "j up\n";}
+      if((event.key.keysym.sym == SDLK_k) && keys[k]) {keys[k] = false; cout << "k up\n";}
+      if((event.key.keysym.sym == SDLK_l) && keys[l]) {keys[l] = false; cout << "l up\n";}
+      if((event.key.keysym.sym == SDLK_m) && keys[m]) {keys[m] = false; cout << "m up\n";}
+      if((event.key.keysym.sym == SDLK_n) && keys[n]) {keys[n] = false; cout << "n up\n";}
+      if((event.key.keysym.sym == SDLK_o) && keys[o]) {keys[o] = false; cout << "o up\n";}
+      if((event.key.keysym.sym == SDLK_p) && keys[p]) {keys[p] = false; cout << "p up\n";}
+      if((event.key.keysym.sym == SDLK_q) && keys[q]) {keys[q] = false; cout << "q up\n";}
+      if((event.key.keysym.sym == SDLK_r) && keys[r]) {keys[r] = false; cout << "r up\n";}
+      if((event.key.keysym.sym == SDLK_s) && keys[s]) {keys[s] = false; cout << "s up\n";}
+      if((event.key.keysym.sym == SDLK_t) && keys[t]) {keys[t] = false; cout << "t up\n";}
+      if((event.key.keysym.sym == SDLK_u) && keys[u]) {keys[u] = false; cout << "u up\n";}
+      if((event.key.keysym.sym == SDLK_v) && keys[v]) {keys[v] = false; cout << "v up\n";}
+      if((event.key.keysym.sym == SDLK_w) && keys[w]) {keys[w] = false; cout << "w up\n";}
+      if((event.key.keysym.sym == SDLK_a) && keys[x]) {keys[x] = false; cout << "x up\n";}
+      if((event.key.keysym.sym == SDLK_y) && keys[y]) {keys[y] = false; cout << "y up\n";}
+      if((event.key.keysym.sym == SDLK_z) && keys[z]) {keys[z] = false; cout << "z up\n";}
+      if((event.key.keysym.sym == SDLK_ESCAPE) && keys[esc]) {keys[esc] = false; cout << "Esc up\n";}
+      if((event.key.keysym.sym == SDLK_1) && keys[1]) {keys[1] = false; cout << "1 up\n";}
+      if((event.key.keysym.sym == SDLK_2) && keys[2]) {keys[2] = false; cout << "2 up\n";}
+      if((event.key.keysym.sym == SDLK_3) && keys[3]) {keys[3] = false; cout << "3 up\n";}
+      if((event.key.keysym.sym == SDLK_4) && keys[4]) {keys[4] = false; cout << "4 up\n";}
+      if((event.key.keysym.sym == SDLK_5) && keys[5]) {keys[5] = false; cout << "5 up\n";}
+      if((event.key.keysym.sym == SDLK_6) && keys[6]) {keys[6] = false; cout << "6 up\n";}
+      if((event.key.keysym.sym == SDLK_7) && keys[7]) {keys[7] = false; cout << "7 up\n";}
+      if((event.key.keysym.sym == SDLK_8) && keys[8]) {keys[8] = false; cout << "8 up\n";}
+      if((event.key.keysym.sym == SDLK_9) && keys[9]) {keys[9] = false; cout << "9 up\n";}
+      if((event.key.keysym.sym == SDLK_0) && keys[0]) {keys[0] = false; cout << "0 up\n";}
+      if((event.key.keysym.sym == SDLK_LSHIFT) && keys[lshift]) {keys[lshift] = false; cout << "left shift up\n";}
+      if((event.key.keysym.sym == SDLK_RSHIFT) && keys[rshift]) {keys[rshift] = false; cout << "right shift up\n";}
+    }
+/*
     switch(event.type) {
       case SDL_QUIT:
         keys[quit] = true; cout << "\n"; break; break;
@@ -107,6 +209,7 @@ void Input::logPress() {
             case SDL_BUTTON_MIDDLE: keys[mousemiddle] = true; cout << "\n"; break;
           }
         }
+*/
         SDL_GetMouseState(&mousex, &mousey);
     }
 }
@@ -114,6 +217,7 @@ void Input::logPress() {
 bool Input::checkKey(int k) {
   return keys[k];
 }
+
 
 bool Input::reset() {
   //NEED TO LOOP THROUGH KEYS[] AND SET THEM ALL TO false
