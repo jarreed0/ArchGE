901c2e62a235c65c604d0c59f5c6820957f2b204
Cody Johns <cjohns@archeantus.net>
2017-03-28 19:54:38 -0500

    Timer Classes

---
+++ engine/LTimer.h
@@ -0,0 +1,36 @@
+#ifndef LTIMER_H
+#define LTIMER_H
+
+//The application time based timer
+class LTimer
+{
+    public:
+        //Initializes variables
+        LTimer();
+
+        //The various clock actions
+        void start();
+        void stop();
+        void pause();
+        void unpause();
+
+        //Gets the timer's time
+        Uint32 getTicks();
+
+        //Checks the status of the timer
+        bool isStarted();
+        bool isPaused();
+
+    private:
+        //The clock time when the timer started
+        Uint32 mStartTicks;
+
+        //The ticks stored when the timer was paused
+        Uint32 mPausedTicks;
+
+        //The timer status
+        bool mPaused;
+        bool mStarted;
+};
+
+#endif //LTIMER_H
